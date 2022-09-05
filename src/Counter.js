b60785d08d465482455f9e5d18708131146c2a43
Robert Adaszewski <adaszewski@protonmail.com>
2022-09-05 17:22:06 +0200

    dodanie licznika dodanych użytkowników plus pole input minimum 3 znaki

---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Counter(props) {
+    return (
+        <div>
+            Liczba użytkowników: <span> {props.liczUser}</span>
+        </div>
+    )
+
+}
+
+export default Counter ;
--- src/Users.js
+++ src/Users.js
@@ -2,27 +2,30 @@ import React, { Component } from 'react';
 import './Users.css';
 import './UsersList';
 import UsersList from './UsersList';
+import Counter from './Counter';
 
 class Users extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            users: []
+            users: [],
+            liczUser: 0
         };
     }
 
     addUser = (event) => {
         event.preventDefault();
 
-        let newUser= {
-            id:Date.now(),
+        let newUser = {
+            id: Date.now(),
             name: this._inputName.value
         }
 
         this.setState((state) => {
             return ({
-                users: state.users.concat(newUser)
+                users: state.users.concat(newUser),
+                liczUser: state.liczUser +1
             })
         })
         this._inputName.value = '';
@@ -30,22 +33,25 @@ class Users extends Component {
 
     removeUser = (userID) => {
         this.setState((state) => {
-            return({
-                users: state.users.filter(user => 
-                    user.id !== userID)
+            return ({
+                users: state.users.filter(user =>
+                    user.id !== userID),
+                liczUser: state.liczUser -1
             });
         })
     }
+
     render() {
         return (
 
             <div className="users-main">
                 <h1> Lista użytkowników</h1>
                 <form onSubmit={this.addUser}>
-                    <input ref={(element) => { this._inputName = element }} type="text"
+                    <input ref={(element) => { this._inputName = element }} type="text" minLength={3}
                         placeholder="Wpisz imię" />
                     <button type="submit">Dodaj</button>
                 </form>
+                <Counter liczUser={this.state.liczUser} />
                 <UsersList usersList={this.state.users} removeUserMethod={this.removeUser} />
             </div>
         );
